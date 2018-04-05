c987bf012de3843cc6015689904c637484ea51b3
okeefed <okeefed@appstate.edu>
2018-04-05 00:37:47 -0400

    Finished Jump

--- Hero.java
+++ Hero.java
@@ -4,24 +4,33 @@ public class Hero{
     State invisibleState;
     State standingState;
     State deadState;
-    
+    State divingState; 
     State state = standingState;
 
     public Hero(){
         jumpingState = new JumpingState(this);
         duckingState = new DuckingState(this);
-        invisibleState = new InvisisbleState(this);
+        invisibleState = new InvisibleState(this);
         standingState = new StandingState(this);
         deadState = new DeadState(this);
+        divingState = new DivingState(this);
 
     }
     
-    public void upArrow(){
-        state.upArrow();
+    public void upPressedArrow(){
+        state.upPressedArrow();
     }
 
-    public void downArrow(){
-        state.downArrow();
+    public void upReleasedArrow(){
+        state.upReleasedArrow();
+    }
+
+    public void downPressedArrow(){
+        state.downPressedArrow();
+    }
+
+    public void downReleasedArrow(){
+        state.downPressedArrow();
     }
 
     public void thrownObject(){
@@ -36,12 +45,40 @@ public class Hero{
         state.firedObject();
     }
 
+    public void esc(){
+        state.esc();
+    }
+
     public void setState(State state){
         this.state = state;
     }
 
-    
-    
+    public State getJumpingState(){
+        return jumpingState;
+    }
+
+    public State getDuckingState(){
+        return duckingState;
+    }
+
+    public State getStandingState(){
+        return standingState();
+    }
+
+    public State getDeadState(){
+        return deadState();
+    }
+
+    public State getDivingState(){
+        return divingState();
+    }
+
+    public State getInvisibleState(){
+        return invisibleState();
+    }
+
+
+       
 
     
 }
--- JumpingState.java
+++ JumpingState.java
@@ -10,11 +10,12 @@ public class JumpingState implements State{
     
     public void upReleasedArrow(){
         System.out.println("You jumped!");
+        hero.setState(hero.getStandingState());
     }
 
     public void downPressedArrow(){
         System.out.println("You are diving!");
-
+        hero.setState(hero.getDivingState());
     }
 
     public void downReleasedArrow(){
@@ -22,5 +23,20 @@ public class JumpingState implements State{
     }
 
     public void thrownObject(){
+        System.out.println("You have been hit");
+        hero.setState(hero.getDeadState());
+    }
+
+    public void rolledObject(){
+        System.out.println("You have jumped over a rolling object!");
+    }
+
+    public void fireObject(){
+        System.out.println("You have jumped over a fire object!");
+    }
+
+    public void esc(){
+        System.out.println("You are invisble and are unable to do anything to I say");
+        hero.setState(hero.getInvisibleState());
     }
 } 
